4a7f9fdbb53a830e66fdffe11f65f24345004cf2
Artem Olchev <solchev04@gmil.com>
2020-11-19 21:30:47 +0300

    Первый коммит

---
+++ scriptLesson01.js
@@ -0,0 +1,11 @@
+const money = 100;
+var income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+alert("Привет проверяющий! Как твои дела?!");
+console.log("'Книга о власти над собой' Тони Роббинс");
+
+
